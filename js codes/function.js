41659ee19894482a7cef604b189392148a7aeafc
Apsara Bishwokarma <apsarabk94@gmail.com>
2024-09-03 19:27:37 +0545

    Create function.js

---
+++ js codes/function.js
@@ -0,0 +1,13 @@
+export default function Icon() {
+  return <></>;
+}
+
+export function ABC() {
+  return <div>ABC</div>;
+}
+
+export const ABC1 = () => {
+  return <div>ABC</div>;
+};
+
+export const ABC12 = () => <div>ABC</div>;
